715f321b59f9d9dfa7431e4a8ca328a8e3ac53e6
허석 <heoseok87@leevi.co.kr>
2021-05-30 05:39:52 +0900

    - DRM이 적용되어 있는 경우, iv값이 없을 때, media sequence로 대체하도록 수정

--- src/extension/extension-background/src/services/m3u8-parser.ts
+++ src/extension/extension-background/src/services/m3u8-parser.ts
@@ -7,38 +7,45 @@ import { buildAbsoluteURL } from "url-toolkit";
 import { v4 } from "uuid";
 
 export const M3u8Parser: IParser = {
-  parseLevelPlaylist(string: string, baseurl: string): Fragment[] {
-    const parser = new Parser();
-    parser.push(string);
-    return parser.manifest.segments.map((segment, index) => ({
-      index,
-      key: segment.key
-        ? {
-            iv: segment.key.iv,
-            uri: buildAbsoluteURL(baseurl, segment.key.uri),
-          }
-        : {
-            iv: null,
-            uri: null,
-          },
-      uri: buildAbsoluteURL(baseurl, segment.uri),
-    }));
-  },
-  parseMasterPlaylist(string: string, baseurl: string): Level[] {
-    const parser = new Parser();
-    
-    parser.push(string);
-    parser.end();
+    parseLevelPlaylist( string: string, baseurl: string ): Fragment[] {
+        const parser = new Parser();
+        parser.push( string );
+        parser.end();
 
-    const playlists = parser.manifest?.playlists ?? [];
-    return playlists.map((playlist, index) => ({
-      index,
-      id: v4(),
-      playlistID: baseurl,
-      bitrate: playlist.attributes.BANDWIDTH,
-      height: playlist.attributes.RESOLUTION?.height,
-      width: playlist.attributes.RESOLUTION?.width,
-      uri: buildAbsoluteURL(baseurl, playlist.uri),
-    }));
-  },
+        let ivTemp = new Uint8Array( 16 );
+        ivTemp[15] = parser.manifest.mediaSequence;
+        // https://stackoverflow.com/a/50684313/3610856
+        // HLS 스펙에 따르면 IV가 없는 경우, EXT-X-MEDIA-SEQUENCE 값으로 대체할 수 있다.
+
+        return parser.manifest.segments.map(( segment, index ) => ( {
+            index,
+            key: segment.key
+                ? {
+                    iv: segment.key.iv ? segment.key.iv : ivTemp,
+                    uri: buildAbsoluteURL( baseurl, segment.key.uri ),
+                }
+                : {
+                    iv: null,
+                    uri: null,
+                },
+            uri: buildAbsoluteURL( baseurl, segment.uri ),
+        } ) );
+    },
+    parseMasterPlaylist( string: string, baseurl: string ): Level[] {
+        const parser = new Parser();
+
+        parser.push( string );
+        parser.end();
+
+        const playlists = parser.manifest ?.playlists ?? [];
+        return playlists.map(( playlist, index ) => ( {
+            index,
+            id: v4(),
+            playlistID: baseurl,
+            bitrate: playlist.attributes.BANDWIDTH,
+            height: playlist.attributes.RESOLUTION ?.height,
+            width: playlist.attributes.RESOLUTION ?.width,
+            uri: buildAbsoluteURL( baseurl, playlist.uri ),
+        } ) );
+    },
 };
